823c2649571524a792dccb7f8f2e0630f579eea4
Mulugeta Hailegnaw <mulugetahail89@gmail.com>
2023-01-06 10:07:02 +0300

    insertionSort

---
+++ insertionSort.py
@@ -0,0 +1,11 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j = j-1
+        arr[j+1] = key
+arr = [6,4,5,6,7,4,3,2]
+insertion_sort(arr)
+print(arr)
