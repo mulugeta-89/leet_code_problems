877e0b139794ac34e37ccb9316ee7a1418750268
Mulugeta Hailegnaw <mulugetahail89@gmail.com>
2023-01-24 20:03:05 +0300

    pascal triangle

---
+++ pascal_triangle.py
@@ -0,0 +1,21 @@
+
+def pascal_triangle(num):
+    all_array = []
+    for i in range(1,num+1):
+        if i == 1:
+            all_array.append([1])
+        if i == 2:
+            all_array.append([1,1])
+        if i > 2:
+            new_arr =[]
+            new_arr.append(1)
+            working_arr = all_array[len(all_array)-1]
+            for i in range(len(working_arr)-1):
+                new_arr.append(working_arr[i] + working_arr[i+1])
+            new_arr.append(1)
+            all_array.append(new_arr)
+    return all_array
+print(pascal_triangle(5))
+        
+    
+            
