68e4f55f6bd189cf9977bef37dacd98a0616b765
mulugeta-89 <mulugetahail89@gmail.com>
2023-04-17 17:14:32 +0300

    decode-string

---
+++ decodeString.py
@@ -0,0 +1,20 @@
+def decoding(s):
+    curr_num = ""
+    num_str = ""
+    stack = []
+    for item in s:
+        if item.isnumeric():
+            curr_num+=item
+        elif item == "[":
+            stack.append(num_str)
+            stack.append(int(curr_num))
+            num_str = ""
+            curr_num = ""
+        elif item == "]":
+            num = stack.pop()
+            stri = stack.pop()
+            num_str = stri + num*num_str
+        else:
+            num_str += item
+    return num_str
+print(decoding("3[ab1[a]]"))
