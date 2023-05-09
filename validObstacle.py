ec28d24476b8ac51af5f0818f391d5a6aad808ba
mulugeta-89 <mulugetahail89@gmail.com>
2023-05-09 09:55:36 +0300

    find-the-longest-valid-obstacle-course-at-each-position

---
+++ validObstacle.py
@@ -0,0 +1,12 @@
+def validObstacle(obstacles):
+    n = len(obstacles)
+    answer = [1] * n
+    lis = []
+    for i, height in enumerate(obstacles):
+        idx = bisect.bisect_right(lis, height)
+        if idx == len(lis):
+            lis.append(height)
+        else:
+            lis[idx] = height
+        answer[i] = idx + 1
+    return answer
