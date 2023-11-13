bd5b7a4232dad8790e6c20441a832b13dc015e4e
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2023-11-13 16:32:05 +0300

    Time: 1154 ms (44.16%), Space: 20.3 MB (67.68%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,17 +5,19 @@ class Solution:
         nums.sort()
         sol = []
         for i in range(len(nums)-2):
-            j = i+1
-            k = len(nums)-1
-            while j < k:
-                sums = nums[i] + nums[j] + nums[k]
-                if sums == 0:
-                    if [nums[i], nums[j], nums[k]] not in sol:
+            if i==0 or nums[i-1] != nums[i]:
+                j = i+1
+                k = len(nums)-1
+                while j < k:
+                    sums = nums[i] + nums[j] + nums[k]
+                    if sums == 0:
                         sol.append([nums[i], nums[j], nums[k]])
-                    j += 1
-                    k -= 1
-                elif sums > 0:
-                    k -= 1
-                else:
-                    j += 1
+                        while j <k and nums[j] == nums[j+1]: j+=1
+                        while j < k and nums[k] == nums[k-1]: k -= 1
+                        j += 1
+                        k -= 1
+                    elif sums > 0:
+                        k -= 1
+                    else:
+                        j += 1
         return sol
