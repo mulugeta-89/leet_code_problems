6b8ef9418c95f7e3022ab32423666f73981dbd42
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2023-11-30 17:12:21 +0300

    Added solution for Palindrome Number

---
+++ leet-code-solution/palindrome-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = str(x)
+        for i in range(len(x)):
+            if x[i] != x[len(x)-i-1]:
+                return False
+        return True 
