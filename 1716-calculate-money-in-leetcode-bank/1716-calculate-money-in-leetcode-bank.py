dec27313c20a2eeb1b96391edb2000ace977c37e
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2023-12-06 19:11:37 +0300

    Time: 33 ms (91.19%), Space: 16.2 MB (73.58%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -8,7 +8,7 @@ class Solution:
             upper.append(days)
             days += 1
         for i in range(len(upper)):
-            sumi += sum([i+1 for i in range(i, upper[i])])
+            sumi += (7 * (upper[i]+(upper[i]-6)) // 2)
         for i in range(num % 7):
             quotient +=1
             sumi += quotient
