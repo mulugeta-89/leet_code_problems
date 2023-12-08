4c8598e0cf0458ed503036fc7b7d22535f07b73c
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2023-12-08 17:19:51 +0300

    Added solution for Missing Number

---
+++ leet-code-solution/missing-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        #nums.sort()
+        #maxi = max(nums)
+        for i in range(len(nums)+1):
+            if i not in nums:
+                return i
+        return -1
+
+        
