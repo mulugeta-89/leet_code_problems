fc0429cbf343b47e432bf38f8d0cca0c5627bb4e
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2023-12-28 19:43:36 +0300

    Added solution for Matrix Diagonal Sum

---
+++ leet-code-solution/matrix-diagonal-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        total = 0
+        count = 0
+        for item in mat:
+            total += item[count]
+            count += 1
+        count2 = len(mat[0])-1
+        for item in mat:
+            total += item[count2]
+            count2 -= 1
+        if(len(mat)%2 != 0):
+            total -= mat[len(mat)//2][len(mat[0])//2]
+        return total
