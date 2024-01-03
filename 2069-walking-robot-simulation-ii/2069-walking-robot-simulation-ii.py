6819403680f864c5119439fb388656c0f816a188
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2024-01-03 18:11:22 +0300

    Time: 229 ms (96.15%), Space: 22 MB (51.44%) - LeetHub

---
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.py
@@ -0,0 +1,16 @@
+class Robot:
+    def __init__(self, width, height):
+        self.isOrigin = True
+        self.i = 0
+        self.pos = [((0,0), "South")] + \
+        [((i,0), "East") for i in range(1, width)] + \
+        [((width-1, j), "North") for j in range(1, height)] + \
+        [((i, height-1), "West") for i in range(width-2, -1, -1)] + \
+        [((0, j), "South") for j in range(height-2, 0, -1)]
+    def step(self, num):
+        self.isOrigin = False
+        self.i = (self.i + num) % len(self.pos)
+    def getPos(self):
+        return self.pos[self.i][0]
+    def getDir(self):
+        return "East" if self.isOrigin else self.pos[self.i][1]
