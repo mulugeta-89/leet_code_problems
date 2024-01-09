619248a63b20161645c6e4fae0af3d50dcf486c6
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2024-01-09 18:06:50 +0300

    Added solution for Sort Colors

--- leet-code-solution/sort-colors.py
+++ leet-code-solution/sort-colors.py
@@ -4,8 +4,8 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         for i in range(len(nums)):
-            for j in range(len(nums)-i-1):
-                if nums[j] > nums[j+1]:
-                    nums[j], nums[j+1] = nums[j+1], nums[j]
-        return nums
-        
+            for j in range(i+1, len(nums)):
+                if nums[i] > nums[j]:
+                    temp = nums[i]
+                    nums[i] = nums[j]
+                    nums[j] = temp
