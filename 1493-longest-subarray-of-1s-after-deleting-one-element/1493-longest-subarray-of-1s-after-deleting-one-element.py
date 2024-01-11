24e7adadbd579146eadb5db1c61c41ecc869376a
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2024-01-11 15:31:08 +0300

    Time: 520 ms (44.80%), Space: 20.5 MB (24.54%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        left = 0
+        maxi = 0
+        freq = {1:0, 0:0}
+        for right in range(len(nums)):
+            freq[nums[right]] += 1
+            while freq[0] > 1:
+                freq[nums[left]] -= 1
+                left += 1
+            maxi = max(maxi, right-left)
+        return maxi
+        
