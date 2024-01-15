f3ebf2f795bbc01489e437af8c96058fcc4e1dfd
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2024-01-15 10:53:25 +0300

    Time: 666 ms (74.20%), Space: 35.6 MB (87.92%) - LeetHub

---
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimumCardPickup(self, cards: List[int]) -> int:
+        left = 0
+        freq = {}
+        sol = float("inf")
+        for right in range(len(cards)):
+            freq[cards[right]] = freq.get(cards[right],0) + 1
+            while freq[cards[right]] == 2:
+                sol = min(sol, right-left+1)
+                freq[cards[left]] -= 1
+                if freq[cards[left]] == 0:
+                    del freq[cards[left]]
+                left += 1
+        return sol if sol != float("inf") else -1
+        
