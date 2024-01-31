b66140257af5d0354892f1fe0f0ac20bc19d59e6
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2024-01-31 14:31:28 +0300

    Add solution for ReplacetheSubstringforBalancedString

---
+++ leet-code-solution/LeetCode/ReplacetheSubstringforBalancedString.py
@@ -0,0 +1,18 @@
+freq = {
+        "W":0,
+        "Q":0,
+        "R":0,
+        "E":0
+    }
+        for i in range(len(s)):
+            freq[s[i]] += 1
+        left = 0
+        count = len(s)/4
+        mini = float("inf")
+        for right in range(len(s)):
+            freq[s[right]] -= 1
+            while left < len(s) and freq["E"] <= count and freq["Q"] <= count and freq["R"] <= count and freq["W"] <= count:
+                mini = min(mini, right-left+1)
+                freq[s[left]] += 1
+                left += 1
+        return mini
