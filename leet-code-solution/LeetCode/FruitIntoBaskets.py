363490dca98e425a1fb911eb7bf93d6a4860ea92
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2024-01-31 13:40:50 +0300

    Add solution for FruitIntoBaskets

---
+++ leet-code-solution/LeetCode/FruitIntoBaskets.py
@@ -0,0 +1,18 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        left = 0
+        maxi = -1
+        count = 0
+        freq = {}
+        for right in range(len(fruits)):
+            if fruits[right] not in freq:
+                count += 1
+            freq[fruits[right]] = freq.get(fruits[right],0) + 1
+            while count > 2:
+                freq[fruits[left]] -= 1
+                if freq[fruits[left]] == 0:
+                    del freq[fruits[left]]
+                    count -= 1
+                left += 1
+            maxi = max(maxi, right-left+1)
+        return maxi
