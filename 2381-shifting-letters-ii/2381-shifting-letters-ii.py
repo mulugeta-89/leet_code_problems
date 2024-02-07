6cba8acde70b8c2bba85874fb1dbba9f60a9f9e7
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2024-02-07 21:54:52 +0300

    Time: 1063 ms (32.35%), Space: 41.7 MB (79.71%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -0,0 +1,48 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+            alphabet_dict = {
+        0: 'a',
+        1: 'b',
+        2: 'c',
+        3: 'd',
+        4: 'e',
+        5: 'f',
+        6: 'g',
+        7: 'h',
+        8: 'i',
+        9: 'j',
+        10: 'k',
+        11: 'l',
+        12: 'm',
+        13: 'n',
+        14: 'o',
+        15: 'p',
+        16: 'q',
+        17: 'r',
+        18: 's',
+        19: 't',
+        20: 'u',
+        21: 'v',
+        22: 'w',
+        23: 'x',
+        24: 'y',
+        25: 'z'
+    }
+            arr = [0] * len(s)
+            for shift in shifts:
+                a,b,k = shift
+                if k == 0:
+                    arr[a] += -1
+                    if b != len(s)-1:
+                        arr[b+1] += 1
+                else:
+                    arr[a] += 1
+                    if b != len(s)-1:
+                        arr[b+1] -= 1
+            for i in range(1, len(arr)):
+                arr[i] = arr[i-1] + arr[i]
+            sol_str = ""
+            for i in range(len(arr)):
+                sol_str += alphabet_dict[(ord(s[i]) - ord('a') + arr[i]) %26]
+            return sol_str
+        
