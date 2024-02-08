c424f01b1e5e86764bc9098c76119b9afbc46888
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2024-02-08 16:16:24 +0300

    Add solution for CorporateFlightBookings

---
+++ leet-code-solution/LeetCode/CorporateFlightBookings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        sol_arr = [0] * (n+1)
+        for a,b,k in bookings:
+            sol_arr[a-1] += k
+            sol_arr[b] -= k
+        for i in range(1, len(sol_arr)):
+            sol_arr[i] = sol_arr[i-1] + sol_arr[i]
+        return sol_arr[:len(sol_arr)-1]
