933ad0f34a48a749a544003d37c24a86890e5e8a
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2024-02-09 11:56:20 +0300

    Time: 691 ms (30.98%), Space: 51.8 MB (6.05%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
@@ -1,18 +1,19 @@
 import numpy as np
 class Solution:
     def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
+        sumi = 0
         sol_arr = []
-        prefix_sum = [nums[0]]
-        for i in range(1, len(nums)):
-            prefix_sum.append(prefix_sum[-1] + nums[i])
+        prefix_sum = [0] * len(nums)
+        suffix_sum = [0] * len(nums)
         for i in range(len(nums)):
-            left_sum = prefix_sum[i] -nums[i]
-            right_sum = prefix_sum[-1] - prefix_sum[i]
-
-            left_count = i
-            right_count = len(nums)-1-i
-
-            left_count_sum =(left_count*nums[i]) - left_sum
-            right_count_sum = right_sum-right_count*nums[i]
-            sol_arr.append(left_count_sum + right_count_sum)
+            prefix_sum[i] = sumi
+            sumi += nums[i]
+        sumi = 0
+        for j in range(len(nums)-1, -1, -1):
+            suffix_sum[j] = sumi
+            sumi += nums[j]
+        for i in range(len(nums)):
+            lower_sum = (nums[i]*i) - prefix_sum[i]
+            upper_sum = (suffix_sum[i]) - (len(nums)-i-1) * nums[i]
+            sol_arr.append(lower_sum+upper_sum)
         return sol_arr
