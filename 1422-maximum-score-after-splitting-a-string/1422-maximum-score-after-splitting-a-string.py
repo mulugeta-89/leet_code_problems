bc34797b43831845f0a085d880f80676bcddc7a5
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2024-02-09 15:14:33 +0300

    Time: 49 ms (14.79%), Space: 16.5 MB (98.92%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        prefix_sum =[0] * (len(s)-1)
+        suffix_sum = [0] * (len(s)-1)
+        count = 0
+        for i in range(len(s)-1):
+            if s[i] == "0":
+                count += 1
+            prefix_sum[i] += count
+        count1 = 0
+        for i in range(len(s)-1, 0, -1):
+            if s[i] == "1":
+                count1 += 1
+            suffix_sum[i-1] += count1
+        maxi = -1
+        for i in range(len(suffix_sum)):
+            sumi = suffix_sum[i]+prefix_sum[i]
+            maxi = max(maxi, sumi)
+        return maxi
+        
