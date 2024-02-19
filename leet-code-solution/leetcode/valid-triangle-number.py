c57f475c7f106eb09705f0dacca8b25b3b3365af
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2024-02-19 10:14:49 +0300

    Add solution for Valid Triangle Number

---
+++ leet-code-solution/leetcode/valid-triangle-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        count = 0
+        for i in range(len(nums)-2):
+            for j in range(i+1, len(nums)-1):
+                ind =  bisect_left(nums, nums[i]+nums[j], lo=j+1)-1
+                count += ind-j
+        return count
+        
