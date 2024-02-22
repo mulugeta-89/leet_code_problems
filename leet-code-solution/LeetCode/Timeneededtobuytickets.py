3106616d834d9568bcfd2c121ef62df6590b5364
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2024-02-22 18:13:36 +0300

    Add solution for Timeneededtobuytickets

---
+++ leet-code-solution/LeetCode/Timeneededtobuytickets.py
@@ -0,0 +1,13 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        count = 0
+        while tickets[k] != 0:
+            mini = 0
+            for i in range(len(tickets)):
+                if tickets[i] != 0:
+                    tickets[i] -= 1
+                    mini += 1
+                if tickets[k] == 0:
+                    break
+            count += mini
+        return count
