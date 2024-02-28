e450a35ea582f964d0fa8a45bd6b1c26f390d4c1
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2024-02-28 15:29:52 +0300

    Add solution for ValidateBinarySearchTree

---
+++ leet-code-solution/LeetCode/ValidateBinarySearchTree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        arr = []
+        def valid(root):
+            if root:
+                valid(root.left)
+                arr.append(root.val)
+                valid(root.right)
+        valid(root)
+        for i in range(1, len(arr)):
+            if arr[i] <= arr[i-1]:
+                return False
+        return True
