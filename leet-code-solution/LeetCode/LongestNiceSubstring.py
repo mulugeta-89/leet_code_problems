e637dd3c4c16de07678135c3a3ef668f0258d747
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2024-03-11 08:06:33 +0300

    Add solution for LongestNiceSubstring

---
+++ leet-code-solution/LeetCode/LongestNiceSubstring.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        maxi = -1
+        sol = ""
+        for i in range(len(s)):
+            temp = s[i]
+            for j in range(i+1, len(s)):
+                temp += s[j]
+                flag = True
+                for k in temp:
+                    small = ""
+                    if k.isupper():
+                        small = k.lower()
+                    if k.islower():
+                        small = k.upper()
+                    if small not in temp:
+                        flag = False
+                if flag and len(temp) > maxi:
+                    maxi = len(temp)
+                    sol = temp
+        return sol
